9b6e6e286ef09de92c1dad64ef1dd9b9a1af6dab
apple <525772188@qq.com>
2018-12-02 14:51:04 +0800

    新增getStudent方法

--- src/main/java/com/wiggin/springboot/demo/controller/StudentController.java
+++ src/main/java/com/wiggin/springboot/demo/controller/StudentController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Date;
+
 /**
  * copyright 2015-2020
  *
@@ -23,5 +25,27 @@ public class StudentController {
     public Student qry(int id){
         System.out.println("this is v1 0");
         return service.queryById(id);
+    }
+
+    @RequestMapping(value = "/qryById")
+    public Student qry(int id){
+        Student student = new Student();
+        student.setId(1);
+        student.setAddress("广东");
+        student.setName("jack");
+        student.setDepartment("学习部");
+        student.setSex("男");
+        return student;
+    }
+
+    @RequestMapping(value = "/getStudent")
+    public Student qry(int id){
+        Student student = new Student();
+        student.setId(2);
+        student.setAddress("广东");
+        student.setBirth(new Date());
+        student.setDepartment("纪检部");
+        student.setSex("男");
+        return student;
     }
 }
