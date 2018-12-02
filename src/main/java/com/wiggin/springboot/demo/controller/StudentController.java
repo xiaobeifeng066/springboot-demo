c84def092157cc1feb760158a87bf5bb6bf28823
apple <525772188@qq.com>
2018-12-02 14:57:12 +0800

    新增输出信息

--- src/main/java/com/wiggin/springboot/demo/controller/StudentController.java
+++ src/main/java/com/wiggin/springboot/demo/controller/StudentController.java
@@ -46,6 +46,7 @@ public class StudentController {
         student.setBirth(new Date());
         student.setDepartment("纪检部");
         student.setSex("男");
+        System.out.println("这是dev2");
         return student;
     }
 }
